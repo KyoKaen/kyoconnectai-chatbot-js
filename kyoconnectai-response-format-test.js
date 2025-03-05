cc6c133518b71cefabc85eada2eed168cf5d86c9
KyoConnectAI <gorightgoleft@gmail.com>
2025-03-05 12:20:47 +0100

    Update kyoconnectai-response-format-test.js

--- kyoconnectai-response-format-test.js
+++ kyoconnectai-response-format-test.js
@@ -114,7 +114,7 @@ class KyoconnectaiHPChatbot {
         bottom: 90px;
         right: 25px;
         width: 420px;
-        max-height: 600px;
+        height: 650px;
         background: white;
         border-radius: 15px;
         box-shadow: 0 2px 10px rgba(0,0,0,0.1);
@@ -642,13 +642,3 @@ if (document.readyState === 'complete') {
 } else {
   window.addEventListener('DOMContentLoaded', () => new KyoconnectaiHPChatbot({ apiUrl }));
 }
-
-// // Initialization
-// if (document.readyState === 'complete') {
-//   new KyoconnectaiHPChatbot({
-//     apiUrl: 'https://kyoconnectai-hp-chatbot-genai-1096582767898.europe-west1.run.app/chat'
-//   });
-
-// } else {
-//   window.addEventListener('DOMContentLoaded', () => new KyoconnectaiHPChatbot({ apiUrl: 'https://kyoconnectai-hp-chatbot-genai-1096582767898.europe-west1.run.app/chat'}));
-// }
