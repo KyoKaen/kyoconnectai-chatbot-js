c93c35553c5990a1463f29b437a3dfa918d0a357
kaenkyo <gorightgoleft@gmail.com>
2026-07-05 18:05:23 +0200

    Remove unused API URL initialization comments
    Removed commented-out API URL initialization code.

--- camping-esplanaadi-chatbot-js/frontend-format-test-cp.js
+++ camping-esplanaadi-chatbot-js/frontend-format-test-cp.js
@@ -628,7 +628,6 @@ class CampingChatbot {
   }
 }
 
-//https://camping-esplanaadi-chatbot-1096582767898.europe-west1.run.app/chat
 // Initialization
 // const SERVICE_NAME="camping-esplanaadi-chatbot"
 // const API_SUFFIX = "-1096582767898.europe-west1.run.app/chat";
